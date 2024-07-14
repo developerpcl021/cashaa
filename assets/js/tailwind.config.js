12147d53ef9a629a18335f25e2e18eb55594994c
aadesh <developerpcl021@gmail.com>
2024-07-14 20:10:08 +0530

    new home

--- assets/js/tailwind.config.js
+++ assets/js/tailwind.config.js
@@ -1,10 +1,17 @@
 tailwind.config = {
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        header: "#1a1f2f",
+        global: "#14192a",
+        clickable: "#3F6FF9",
+      },
       fontFamily: {
         dmsans: "DM Sans",
       },
+      fontSize: {
+        'main-head': "55px"
+      }
     },
   },
 };
